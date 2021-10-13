615ecd9170f426c15979ed5cf384de368e73887d
Duy Anh <duyanhhack@gmail.com>
2021-10-13 16:56:03 +0700

    music

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+    {
+        name: 'Beat It',
+        path: './Assets/Musics/Beat-It.mp3',
+        artist: 'Michael Jackson',
+        cover: './Assets/Images/beat-it.jpg'
+    },
+    {
+        name: 'Billie Jean',
+        path: './Assets/Musics/Billie-Jean.mp3',
+        artist: 'Michael Jackson',
+        cover: './Assets/Images/billie-jean.jpg'
+    },
+    {
+        name: 'Đã Lỡ Yêu Em Nhiều',
+        path: './Assets/Musics/Da-Lo-Yeu-Em-Nhieu.mp3',
+        artist: 'JustaTee',
+        cover: './Assets/Images/da-lo-yeu-em-nhieu.jpg'
+    },
+    {
+        name: 'Em Gái Mưa',
+        path: './Assets/Musics/Em-Gai-Mua.mp3',
+        artist: 'Hương Tràm',
+        cover: './Assets/Images/em-gai-mua.jpg'
+    },
+    {
+        name: 'Gác Lại Âu Lo Remix',
+        path: './Assets/Musics/Gac-Lai-Au-Lo-REMIX.mp3',
+        artist: 'DA LAB X MIU LÊ ( ZANG REMIX )',
+        cover: './Assets/Images/gac-lai-au-lo.jpg'
+    },
+    {
+        name: 'In My Mind',
+        path: './Assets/Musics/In-My-Mind.mp3',
+        artist: "Dynoro & Gigi D'Agostino",
+        cover: './Assets/Images/in-my-mind.jpg'
+    },
+    {
+        name: "L'Amour Toujours",
+        path: './Assets/Musics/LAmour-Toujours.mp3',
+        artist: "Gigi D'Agostino ",
+        cover: './Assets/Images/LAmour-Toujours.jpg'
+    },
+    {
+        name: 'Sài Gòn Hôm Nay Mưa',
+        path: './Assets/Musics/Sai-Gon-Hom-Nay-Mua.mp3',
+        artist: 'JSOL & Hoàng Duyên',
+        cover: './Assets/Images/sai-gon-hom-nay-mua.jpg'
+    },
+];
